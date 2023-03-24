17106ddc2db390a696e4ec65c0fcbeb373abfb9f
erlzhang <zhangshiyu1992@hotmail.com>
2023-03-24 16:48:42 +0800

    dehug

--- opgauss.go
+++ opgauss.go
@@ -47,7 +47,8 @@ func (dialector Dialector) Initialize(db *gorm.DB) (err error) {
 	// register callbacks
 	if !dialector.WithoutReturning {
 		callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{
-			CreateClauses: []string{"INSERT", "VALUES", "ON CONFLICT", "RETURNING"},
+			//CreateClauses: []string{"INSERT", "VALUES", "ON CONFLICT", "RETURNING"},
+			CreateClauses: []string{"INSERT", "VALUES", "ON CONFLICT"},
 			UpdateClauses: []string{"UPDATE", "SET", "WHERE", "RETURNING"},
 			DeleteClauses: []string{"DELETE", "FROM", "WHERE", "RETURNING"},
 		})
